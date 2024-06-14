6cd6147419a77ed7c9f07027f14f026982c959a7
Yerragorla Ganesh Yadav <yerragorlaganeshyadav@Yerragorlas-MacBook-Pro.local>
2024-06-14 15:19:36 +0530

    Initial commit

---
+++ src/main/java/com/watchandearn/WatchAndEarnApplication.java
@@ -0,0 +1,13 @@
+package com.watchandearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchAndEarnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchAndEarnApplication.class, args);
+	}
+
+}
